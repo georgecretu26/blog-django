f6368852f5638fce5bff9a1825e6a77c3c3e68f8
georgecretu26 <georgecretu@Georges-MacBook-Pro.local>
2018-03-03 00:47:18 +0100

    added urls to the articles

--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
 
@@ -8,3 +8,8 @@ from .models import Article
 class ArticleListView(ListView):
     model = Article
     template_name = 'home.html'
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'detail.html'
+    context_object_name = 'batman'
